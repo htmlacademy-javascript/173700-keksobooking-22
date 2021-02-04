1f6626cc4f7f463bd527aad2754da7b18fc50046
aoihikaru <aoi.hikaru13@gmail.com>
2021-02-04 15:50:11 +0300

    Исправлена ошибка, выдаваемая линтером

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // исходная версия Math.random() * (lastNumber - firstNumber) + firstNumber (с изменёнными переменными) взята с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-const getRandomIntegerNumber = (firstNumber, lastNumber) => {
+// const getRandomIntegerNumber
+(firstNumber, lastNumber) => {
   if (firstNumber >= 0, lastNumber >= 0) {
     if (firstNumber <= lastNumber) {
       return Math.round(Math.random() * (lastNumber - firstNumber) + firstNumber);
@@ -9,7 +9,8 @@ const getRandomIntegerNumber = (firstNumber, lastNumber) => {
   }
 };
 
-const getRandomDecimalNumber = (firstNumber, lastNumber, decimalPlaces) => {
+// const getRandomDecimalNumber
+(firstNumber, lastNumber, decimalPlaces) => {
   let totalNumber;
   if (firstNumber >= 0, lastNumber >= 0) {
     if (firstNumber <= lastNumber) {
