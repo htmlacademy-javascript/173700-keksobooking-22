cec57cbb8a5a94a44f6476bef16b16702604d94b
Keks <keks@htmlacademy.ru>
2021-02-04 15:01:49 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,21 @@
+// исходная версия Math.random() * (lastNumber - firstNumber) + firstNumber (с изменёнными переменными) взята с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const getRandomIntegerNumber = (firstNumber, lastNumber) => {
+  if (firstNumber >= 0, lastNumber >= 0) {
+    if (firstNumber <= lastNumber) {
+      return Math.round(Math.random() * (lastNumber - firstNumber) + firstNumber);
+    }
+    return Math.round(Math.random() * (firstNumber - lastNumber) + lastNumber);
+  }
+};
+
+const getRandomDecimalNumber = (firstNumber, lastNumber, decimalPlaces) => {
+  let totalNumber;
+  if (firstNumber >= 0, lastNumber >= 0) {
+    if (firstNumber <= lastNumber) {
+      totalNumber = Math.random() * (lastNumber - firstNumber) + firstNumber;
+    }
+    totalNumber = Math.random() * (firstNumber - lastNumber) + lastNumber;
+  }
+  return +(totalNumber.toFixed(decimalPlaces));
+};
